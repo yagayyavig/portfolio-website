e704ae52b9c586a5bb7489ce72858e4796831883
yagayyavig <yagayya.vig.dev@gmail.com>
2026-03-07 14:06:34 -0800

    new updates

--- python2/Store/models/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ["Customer", "Product", "Order", "ProductOrder", "Category"]
-
-from .customer import Customer
-from .product import Product
-from .order import Order
-from .product_order import ProductOrder
-from .category import Category
